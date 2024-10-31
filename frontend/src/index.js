190767efcd841b0a05006a36e931ea25d10c7868
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2024-10-31 14:52:00 +0330

    route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './assets/styles/index.css';
-import './assets/styles/bootstrap.custom.css';
-import {} from {react-router-dom}
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/styles/index.css";
+import "./assets/styles/bootstrap.custom.css";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element ={<App/>}>
+      <Route index={true} path="/" element= {<HomeScreen/>}/>
+    </Route>
+  )
+)
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
