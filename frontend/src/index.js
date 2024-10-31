89f904787e70349af4d630d8cc43133004ddb72f
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2024-10-31 10:56:16 +0330

    bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
 import './assets/styles/bootstrap.custom.css';
+import {} from {react-router-dom}
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
