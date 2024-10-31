ebf721aea15170ea91c742894fe428f88593e2da
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2024-10-31 10:40:38 +0330

    bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import Header from './components/Header'
-import { Container } from 'react-bootstrap'
-import Footer from './components/Footer'
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 const App = () => {
   return (
     <>
       <Header/>
       <main className='py-3'>
-        <Container>
-          <h1> Welcome To MehrShop</h1>
-        </Container>
+      <Container>
+        <HomeScreen/>
+      </Container>
       </main>
+      
       <Footer/>
 
       
